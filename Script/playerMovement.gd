85a607dc4d274857368f7089a1bdae8453cc289f
ShawnWin123 <ongshawnwin@gmail.com>
2025-03-11 22:00:06 +0800

    sdsds

---
+++ Script/playerMovement.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var speed = 400.0
+@export var dashSpeed = 1600
+@export_range(0,1) var deceleration = 0.4
+@export_range(0,1) var acceleration = 0.4
+@export var jumpForce= -620
+@export_range(0,1) var decelerationOnJumpRelease = 0.5
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var lastDireaction: int
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		if velocity.y < 0:
+			velocity.y += gravity * delta
+		else:
+			velocity.y += (gravity + 200) * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jumpForce
+		
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= decelerationOnJumpRelease
+
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
+		animated_sprite_2d.animation = "walk"
+		lastDireaction = direction
+		if direction == 1:
+			animated_sprite_2d.flip_h = false
+		else:
+			animated_sprite_2d.flip_h = true
+			
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed * deceleration)
+		animated_sprite_2d.animation = "idle"
+		
+	if Input.is_action_just_pressed("dash"):
+		velocity.x += dashSpeed * lastDireaction
+		velocity.y = 0
+		
+
+	move_and_slide()
---
+++ Script/room_based_camera.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+@onready var screenSize : Vector2 = get_viewport_rect().size
+@onready var playerNode: CharacterBody2D = $"../Player"
+
+
+func _ready():
+	set_screen_position()
+	await get_tree().process_frame
+	position_smoothing_enabled = true
+	position_smoothing_speed = 10
+	
+func _process(delta: float) -> void:
+	set_screen_position()
+	
+func set_screen_position():
+	var playerPos = playerNode.global_position
+	var x = floor(playerPos.x / screenSize.x) * screenSize.x + screenSize.x/2
+	var y = floor(playerPos.y / screenSize.y) * screenSize.y + screenSize.y/2
+	global_position = Vector2(x,y)
